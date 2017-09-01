d5fcb6000d8fb090b037d6e900bf023da1e750b9
Amanda Mayara <amanda.mayaraa@gmail.com>
2017-09-01 10:52:48 -0300

    Criando e centralizando novas funcoes de listagem de produtos

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,12 +1,10 @@
-<?php include("cabecalho.php"); ?>
-<?php include("conecta.php"); ?>
+<?php 
+	include("cabecalho.php");
+	include("conecta.php"); 
+	include("banco-produto.php");?>
 
 <?php
 
-function insereProduto($conexao, $nome, $preco) {
-    $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
-    return mysqli_query($conexao, $query);
-}
 
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
